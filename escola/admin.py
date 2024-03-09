10d16c6f92a8d92fb6af94b1128042ba77545d43
L F <franco100lucas@gmail.com>
2024-03-09 10:47:28 -0300

    feat: Creating admin module

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome') # Nome dos elementos que vão poder ser selecionáveis
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+# admin.site.register(nome_da_classe, nome_da_classe_admin)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao', 'nivel')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso', 'nivel')
+
+admin.site.register(Curso, Cursos)
