ae88d0d7ff7575219c7d677839c535d1b87726b4
L F <franco100lucas@gmail.com>
2024-03-10 12:36:08 -0300

    refactor: Adding router and changing path to index

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+# ROUTER é uma forma de agrupar as views comuns em um único local
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+# router.register('path', ViewSet, basename='Nome')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos, name='alunos'), 
+    path('', include(router.urls) ), # Inclui as rotas do router
 ]
