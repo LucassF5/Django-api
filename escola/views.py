627b15d051338e0c2304ff8bc75f18b74813fe15
L F <franco100lucas@gmail.com>
2024-03-10 12:34:46 -0300

    refactor: Change file to use ViewSets

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,14 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
+# A viewset é uma forma de agrupar as views comuns em um único local
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos"""
+    queryset = Aluno.objects.all() # Recebe todos os objetos do model Aluno
+    serializer_class = AlunoSerializer # Classe responsável por serializar os dados
 
-def alunos(request):
-    if request.method == "GET":
-        aluno = {'id':1, 'nome':'Lucas'}
-        return JsonResponse(aluno)
-
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all() # Recebe todos os objetos do model Curso
+    serializer_class = CursoSerializer # Classe responsável por serializar os dados
