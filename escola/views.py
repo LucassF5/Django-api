ef6d2bfcf1e33fd2225427aad469dca80ef57055
L F <franco100lucas@gmail.com>
2024-03-12 03:03:02 -0300

    feat: Creating authentication method

--- escola/views.py
+++ escola/views.py
@@ -1,34 +1,51 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # A viewset é uma forma de agrupar as views comuns em um único local
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
-    queryset = Aluno.objects.all() # Recebe todos os objetos do model Aluno
-    serializer_class = AlunoSerializer # Classe responsável por serializar os dados
+    queryset = Aluno.objects.all()  # Recebe todos os objetos do model Aluno
+    serializer_class = AlunoSerializer  # Classe responsável por serializar os dados
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
-    queryset = Curso.objects.all() # Recebe todos os objetos do model Curso
-    serializer_class = CursoSerializer # Classe responsável por serializar os dados
+    queryset = Curso.objects.all()  # Recebe todos os objetos do model Curso
+    serializer_class = CursoSerializer  # Classe responsável por serializar os dados
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class MatriculasViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class ListaMatriculasAluno(generics.ListAPIView):
-# ListAPIView é uma classe que fornece uma implementação básica para a listagem de dados
+    # ListAPIView é uma classe que fornece uma implementação básica para a listagem de dados
     """Listando as matrículas de um aluno ou aluna"""
     def get_queryset(self):
         # Método que retorna o queryset que será utilizado
-        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk']) # Filtra as matrículas pelo id do aluno
+        # Filtra as matrículas pelo id do aluno
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class ListaAlunosMatriculados(generics.ListAPIView):
     """Listando alunos matriculados em um curso"""
     def get_queryset(self):
-        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
